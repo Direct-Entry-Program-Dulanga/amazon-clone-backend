94af0ed80af1aa88f44fe51aab83ac7d31187ef8
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-11-09 22:07:40 +0530

    Finalize and test the placeOrder and OrderService

--- src/test/java/lk/ijse/amazonclonebackend/service/OrderServiceTest.java
+++ src/test/java/lk/ijse/amazonclonebackend/service/OrderServiceTest.java
@@ -1,22 +1,57 @@
 package lk.ijse.amazonclonebackend.service;
 
+import lk.ijse.amazonclonebackend.dto.ItemDTO;
+import lk.ijse.amazonclonebackend.dto.OrderDetailDTO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    private OrderService orderService;
+    private ItemService itemService;
+    private Connection connection;
 
-    @AfterEach
-    void tearDown() {
+    @BeforeEach
+    void setUp() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dep7_amazon_clone", "root", "test");
+        this.orderService = new OrderService(connection);
+        this.itemService = new ItemService(connection);
     }
 
     @Test
     void placeOrder() {
+        ItemDTO i001 = itemService.getItem("I001");
+        ItemDTO i002 = itemService.getItem("I002");
+
+        List<OrderDetailDTO> orderDetails = new ArrayList<>();
+        orderDetails.add(new OrderDetailDTO("I001", 2, i001.getUnitPrice()));
+        orderDetails.add(new OrderDetailDTO("I002", 1, i002.getUnitPrice()));
+
+        assertDoesNotThrow(() -> {
+            orderService.placeOrder(orderDetails);
+        });
+
+        ItemDTO afterTestI001 = itemService.getItem("I001");
+        ItemDTO afterTestI002 = itemService.getItem("I002");
+
+        assertEquals(i001.getQty() - 2, afterTestI001.getQty());
+        assertEquals(i002.getQty() - 1, afterTestI002.getQty());
     }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        connection.close();
+    }
+
 }
